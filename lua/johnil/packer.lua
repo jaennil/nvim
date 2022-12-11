fe3e3884e6ad387d8446270303d4ccc7e58432b5
johnil <ssushnost@gmail.com>
2022-12-11 05:33:20 +0300

    feat: add linter

--- lua/johnil/packer.lua
+++ lua/johnil/packer.lua
@@ -1,23 +1,25 @@
-return require('packer').startup(function(use)
-    use 'wbthomason/packer.nvim'
-    use 'github/copilot.vim'
-    use 'neovim/nvim-lspconfig' -- Configurations for Nvim LSP
-    use 'mhartington/formatter.nvim' -- Format code
-    use {
-        'nvim-treesitter/nvim-treesitter',
-        run = ':TSUpdate'
-    }
-    use 'nvim-tree/nvim-web-devicons'
-    use 'navarasu/onedark.nvim'
-    use {
-    'numToStr/Comment.nvim',
-    config = function()
-        require('Comment').setup()
-    end
-}
-    use {
-        'nvim-telescope/telescope.nvim', tag = '0.1.0',
-        -- or                            , branch = '0.1.x',
-        requires = { {'nvim-lua/plenary.nvim'} }
-}
+return require("packer").startup(function(use)
+	use("wbthomason/packer.nvim")
+	use("github/copilot.vim")
+	use("neovim/nvim-lspconfig") -- Configurations for Nvim LSP
+	use("mhartington/formatter.nvim") -- Format code
+	use("mfussenegger/nvim-lint")
+	use({
+		"nvim-treesitter/nvim-treesitter",
+		run = ":TSUpdate",
+	})
+	use("nvim-tree/nvim-web-devicons")
+	use("navarasu/onedark.nvim")
+	use({
+		"numToStr/Comment.nvim",
+		config = function()
+			require("Comment").setup()
+		end,
+	})
+	use({
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.0",
+		-- or                            , branch = '0.1.x',
+		requires = { { "nvim-lua/plenary.nvim" } },
+	})
 end)
