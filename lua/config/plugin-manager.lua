8a9632dba83efc10711c858c2f385ba2d9e81c76
ssushnost <ssushnost@gmail.com>
2023-07-17 02:43:56 +0300

    feat(plugin): tmux vim navigator

--- lua/config/plugin-manager.lua
+++ lua/config/plugin-manager.lua
@@ -1,26 +1,31 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+    vim.fn.system({
+	"git",
+	"clone",
+	"--filter=blob:none",
+	"https://github.com/folke/lazy.nvim.git",
+	"--branch=stable", -- latest stable release
+	lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 -- Example using a list of specs with the default options
 
-require("lazy").setup({{
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function () 
-      local configs = require("nvim-treesitter.configs")
+require("lazy").setup({
+    {"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function () 
+	    local configs = require("nvim-treesitter.configs")
 
-      configs.setup({
-          highlight = { enable = true },
-          indent = { enable = true },  
-        })
-    end
- }})
+	    configs.setup({
+		highlight = { enable = true },
+		indent = { enable = true },  
+	    })
+	end
+    },
+    {
+	"christoomey/vim-tmux-navigator",
+	lazy = false,
+    }
+})
