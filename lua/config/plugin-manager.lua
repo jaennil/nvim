426ed3cfae077bc5880756d1398a3f959e9147a8
ssushnost <ssushnost@gmail.com>
2023-07-17 03:07:41 +0300

    style(plugin-manager): formatting

--- lua/config/plugin-manager.lua
+++ lua/config/plugin-manager.lua
@@ -12,22 +12,21 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-    {"nvim-treesitter/nvim-treesitter",
+    { "nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
 	config = function () 
 	    local configs = require("nvim-treesitter.configs")
 
 	    configs.setup({
 		highlight = {
-		enable = true,
-		additional_vim_regex_highlighting = false
+		    enable = true,
+		    additional_vim_regex_highlighting = false
 		},
 		indent = { enable = true },  
 	    })
 	end
     },
-    {
-	"christoomey/vim-tmux-navigator",
+    { "christoomey/vim-tmux-navigator",
 	lazy = false,
     }
 })
