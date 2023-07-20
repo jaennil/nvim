873d5b71257910b7eb1465d5cf26b6f2c24e2f57
ssushnost <ssushnost@gmail.com>
2023-07-20 22:00:42 +0300

    fix(colorizer): brokes on source. bind to reload

--- lua/config/keybindings.lua
+++ lua/config/keybindings.lua
@@ -1,3 +1,17 @@
+-- Modes
+--   normal_mode = "n",
+--   insert_mode = "i",
+--   visual_mode = "v",
+--   visual_block_mode = "x",
+--   term_mode = "t",
+--   command_mode = "c",
 vim.g.mapleader = ' '
-vim.api.nvim_set_keymap('n', '<leader>e', ':Explore<CR>', { silent = true })
-vim.api.nvim_set_keymap('n', "<leader><leader>", ":w<CR>:so<CR>", {silent = true })
+local keymap = vim.api.nvim_set_keymap
+keymap('n', '<leader>e', ':Explore<CR>', { silent = true })
+keymap('n', "<leader><leader>", ":w<CR>:so<CR>", {silent = true })
+function reload_colorizer()
+    package.loaded['colorizer'] = nil
+    require('colorizer').setup()
+    require('colorizer').attach_to_buffer(0)
+end
+keymap('n', "<leader>rc", ":lua reload_colorizer()<CR>", {silent = true})
