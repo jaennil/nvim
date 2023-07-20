84576acfce581ace1f1cb9c943339cb847201087
ssushnost <ssushnost@gmail.com>
2023-07-20 21:55:33 +0300

    feat(settings): black sign column

--- lua/config/settings.lua
+++ lua/config/settings.lua
@@ -8,8 +8,8 @@ vim.opt.backup = false
 vim.opt.writebackup = false
 vim.opt.signcolumn = "yes"
 vim.cmd([[
-  highlight SignColumn guibg=NONE
-  highlight SignColumnSB guibg=NONE
+  highlight SignColumn guibg='#000000'
+  highlight SignColumnSB guibg='#000000'
 ]])
 vim.g.netrw_banner = false
 vim.api.nvim_set_option("clipboard","unnamed")
