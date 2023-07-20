de2ba25b30c11440b0db1ad49e65b0b0c01eefcb
ssushnost <ssushnost@gmail.com>
2023-07-20 21:46:07 +0300

    feat(keybindings): bind to save and source file

--- lua/config/keybindings.lua
+++ lua/config/keybindings.lua
@@ -1,2 +1,3 @@
 vim.g.mapleader = ' '
 vim.api.nvim_set_keymap('n', '<leader>e', ':Explore<CR>', { silent = true })
+vim.api.nvim_set_keymap('n', "<leader><leader>", ":w<CR>:so<CR>", {silent = true })
