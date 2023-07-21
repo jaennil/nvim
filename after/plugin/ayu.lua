c69be4067f56a3530f01ae6ee8177e265088c851
ssushnost <ssushnost@gmail.com>
2023-07-21 23:07:46 +0300

    style(colorscheme): formatting

--- after/plugin/ayu.lua
+++ after/plugin/ayu.lua
@@ -8,20 +8,20 @@ local pink = "#ff66c4"
 local yellow = "#ffff00"
 
 require('ayu').setup({
-    overrides = {
-	Normal = {bg = black },
-	-- function name
-	Function = { fg = yellow },
-	-- special words like require
-	Special = { fg = red_orange },
-	Statement = { fg = red_orange },
-	-- imports
-	PreProc = { fg = brown },
-	Constant = { fg = green },
-	Conditional = { fg = purple },
-	String  = {fg = bright_green },
-	['@variable'] = {fg = pink },
-    }
+	overrides = {
+		Normal        = { bg = black },
+		-- function name
+		Function      = { fg = yellow },
+		-- special words like require
+		Special       = { fg = red_orange },
+		Statement     = { fg = red_orange },
+		-- imports
+		PreProc       = { fg = brown },
+		Constant      = { fg = green },
+		Conditional   = { fg = purple },
+		String        = { fg = bright_green },
+		['@variable'] = { fg = pink },
+	}
 })
 require('ayu').colorscheme()
 vim.cmd([[
