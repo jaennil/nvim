22cbab3bf6f17791a8f47be7cc23e70b28d4bc1c
ssushnost <ssushnost@gmail.com>
2023-07-21 22:29:05 +0300

    style(plug-manager): format

--- lua/config/plugin-manager.lua
+++ lua/config/plugin-manager.lua
@@ -1,49 +1,55 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-	"git",
-	"clone",
-	"--filter=blob:none",
-	"https://github.com/folke/lazy.nvim.git",
-	"--branch=stable", -- latest stable release
-	lazypath,
-    })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-    { "nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-    },
-    { "christoomey/vim-tmux-navigator",
-	lazy = false,
-    },
-    { "neovim/nvim-lspconfig" },
-    { 'neoclide/coc.nvim',
-	branch = 'release'
-    },
-    { 'numToStr/Comment.nvim' },
-    { "mbbill/undotree" },
-    { "eandrju/cellular-automaton.nvim" },
-    { "tpope/vim-fugitive" },
-    { "folke/which-key.nvim",
-	event = "VeryLazy",
-	init = function()
-	    vim.o.timeout = true
-	    vim.o.timeoutlen = 700
-	end,
-	opts = {
-	    -- your configuration comes here
-	    -- or leave it empty to use the default settings
-	    -- refer to the configuration section below
-	}
-    },
-    { "shatur/neovim-ayu" },
-    { "norcalli/nvim-colorizer.lua" },
-    { "williamboman/mason.nvim",
-	build = ":MasonUpdate" -- :MasonUpdate updates registry contents
-    },
-    { "nvim-lualine/lualine.nvim",
-	dependencies = { "nvim-tree/nvim-web-devicons" },
-    },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+	},
+	{
+		"christoomey/vim-tmux-navigator",
+		lazy = false,
+	},
+	{ "neovim/nvim-lspconfig" },
+	{
+		'neoclide/coc.nvim',
+		branch = 'release'
+	},
+	{ 'numToStr/Comment.nvim' },
+	{ "mbbill/undotree" },
+	{ "eandrju/cellular-automaton.nvim" },
+	{ "tpope/vim-fugitive" },
+	{
+		"folke/which-key.nvim",
+		event = "VeryLazy",
+		init = function()
+			vim.o.timeout = true
+			vim.o.timeoutlen = 700
+		end,
+		opts = {
+			-- your configuration comes here
+			-- or leave it empty to use the default settings
+			-- refer to the configuration section below
+		}
+	},
+	{ "shatur/neovim-ayu" },
+	{ "norcalli/nvim-colorizer.lua" },
+	{
+		"williamboman/mason.nvim",
+		build = ":MasonUpdate" -- :MasonUpdate updates registry contents
+	},
+	{
+		"nvim-lualine/lualine.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+	},
 })
