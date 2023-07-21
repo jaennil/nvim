90ecadf8be615771143fa29b9eaf844cbb937af5
ssushnost <ssushnost@gmail.com>
2023-07-21 20:20:49 +0300

    feat(lsp): lua

--- after/plugin/lspconfig.lua
+++ after/plugin/lspconfig.lua
@@ -1,2 +1,14 @@
 local lspconfig = require('lspconfig')
 lspconfig.pyright.setup {}
+lspconfig.lua_ls.setup {
+  settings = {
+    Lua = {
+      runtime = {
+        version = 'LuaJIT',
+      },
+      diagnostics = {
+        globals = {'vim'},
+      },
+    },
+  },
+}
