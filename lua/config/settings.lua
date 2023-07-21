a8d8e489f615fbad213891e809bc8a817236c638
ssushnost <ssushnost@gmail.com>
2023-07-21 22:27:56 +0300

    feat(settings): tabstap = 4

--- lua/config/settings.lua
+++ lua/config/settings.lua
@@ -1,6 +1,7 @@
 vim.opt.number = true
 vim.opt.relativenumber = true
-vim.opt.shiftwidth = 4 
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
 vim.opt.hlsearch = false
 vim.opt.termguicolors = true
 vim.opt.updatetime = 50
