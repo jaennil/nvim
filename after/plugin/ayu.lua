eb48a55a93d6a5c0bc3f366687933b14ee6527c2
ssushnost <ssushnost@gmail.com>
2023-07-21 22:04:53 +0300

    feat(theme): custom highlighting

--- after/plugin/ayu.lua
+++ after/plugin/ayu.lua
@@ -1,1 +1,29 @@
+local red_orange = "#ff4f00"
+local black = "#000000"
+local green = "#00ff00"
+local brown = "#f4a460"
+local purple = "#ac64fb"
+local bright_green = "#aaf94c"
+local pink = "#ff66c4"
+
+require('ayu').setup({
+    overrides = {
+	Normal = {bg = black },
+	-- function name
+        Function = { fg = red_orange },
+	-- special words like require
+	Special = { fg = red_orange },
+	Statement = { fg = red_orange },
+	-- imports
+	PreProc = { fg = brown },
+	Constant = { fg = green },
+	Conditional = { fg = purple },
+	String  = {fg = bright_green },
+	['@variable'] = {fg = pink },
+    }
+})
 require('ayu').colorscheme()
+vim.cmd([[
+  highlight SignColumn guibg='#000000'
+  highlight SignColumnSB guibg='#000000'
+]])
--- lua/config/settings.lua
+++ lua/config/settings.lua
@@ -7,11 +7,7 @@ vim.opt.updatetime = 50
 vim.opt.backup = false
 vim.opt.writebackup = false
 vim.opt.signcolumn = "yes"
-vim.cmd([[
-  highlight SignColumn guibg='#000000'
-  highlight SignColumnSB guibg='#000000'
-]])
 vim.g.netrw_banner = false
 vim.api.nvim_set_option("clipboard","unnamed")
 vim.opt.swapfile = false
-vim.cmd([[let w:m1=matchadd('ErrorMsg', '\%>80v.\+', -1)]])
+vim.cmd([[let w:m1=matchadd('Error', '\%>80v.\+', -1)]])
