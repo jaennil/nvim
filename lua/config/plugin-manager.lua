dc995ef66ed9f11f4660ada865b54b7f76e1641e
jaennil <jaennil@yandex.ru>
2024-02-20 00:52:47 +0300

    go plugin

--- lua/config/plugin-manager.lua
+++ lua/config/plugin-manager.lua
@@ -37,7 +37,21 @@ require("lazy").setup({
 	{ "mfussenegger/nvim-dap" },
 	{ "rcarriga/nvim-dap-ui" },
 	{ "leoluz/nvim-dap-go" },
-	{ "folke/neodev.nvim",    opts = {} },
+	{
+		"ray-x/go.nvim",
+		dependencies = { -- optional packages
+			"ray-x/guihua.lua",
+			"neovim/nvim-lspconfig",
+			"nvim-treesitter/nvim-treesitter",
+		},
+		config = function()
+			require("go").setup()
+		end,
+		event = { "CmdlineEnter" },
+		ft = { "go", 'gomod' },
+		build = ':lua require("go.install").update_all_sync()' -- if you need to install/update all binaries
+	},
+	{ "folke/neodev.nvim", opts = {} },
 
 	{ "tpope/vim-fugitive" },
 	{
