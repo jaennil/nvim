53224e9b3b4eb9d96c80e7d210b35a694d50cb2b
jaennil <jaennil@yandex.ru>
2024-06-11 12:41:54 +0300

    feat: rust debugger

--- after/plugin/dap.lua
+++ after/plugin/dap.lua
@@ -125,3 +125,26 @@ dap.configurations.cs = {
 		end
 	}
 }
+
+dap.adapters.codelldb = {
+  type = 'server',
+  port = "${port}",
+  executable = {
+    -- Change this to your path!
+    command = 'codelldb',
+    args = {"--port", "${port}"},
+  }
+}
+
+dap.configurations.rust= {
+  {
+    name = "Launch file",
+    type = "codelldb",
+    request = "launch",
+    program = function()
+      return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+    end,
+    cwd = '${workspaceFolder}',
+    stopOnEntry = false,
+  },
+}
