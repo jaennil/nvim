0bada8bdfe52774cf34523fde603b1fa02ac1bd3
jaennil <jaennil@yandex.ru>
2024-07-20 15:54:41 +0300

    refactor; highlight yanked; some options

--- init.lua
+++ init.lua
@@ -1,1 +1,9 @@
-require("config")
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+vim.g.have_nerd_font = true
+
+require 'options'
+require 'keymaps'
+require 'lazy_bootstrap'
+require 'lazy_plugins'
---
+++ lua/options.lua
@@ -0,0 +1,33 @@
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+-- TODO
+-- vim.opt.mouse = 'a'
+
+vim.opt.tabstop = 8
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+
+vim.opt.termguicolors = true
+
+vim.opt.showmode = false
+
+vim.opt.clipboard = 'unnamedplus'
+
+vim.opt.undofile = true
+
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+vim.opt.signcolumn = 'yes'
+
+vim.opt.updatetime = 2000
+
+vim.opt.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+
+vim.opt.inccommand = 'split'
+
+vim.opt.cursorline = true
+
+vim.opt.scrolloff = 10
