1ba2ec0cf5918f277f24b46468bdf89b5c745b54
jaennil <jaennil@yandex.ru>
2024-07-23 00:02:13 +0300

    feat: lazy load treesitter

--- lua/jaennil/plugins/treesitter.lua
+++ lua/jaennil/plugins/treesitter.lua
@@ -1,23 +1,27 @@
-return {
+local M = {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
-    lazy = false,
-    opts = {
-	ensure_installed = { "bash", "diff", "lua", "markdown", "rust", "go" },
-	sync_install = false,
-	auto_install = true,
-	ignore_install = { },
-	highlight = {
-	    enable = true,
-	    disable = { },
-	    additional_vim_regex_highlighting = false,
-	},
-	indent = {
-	    enable = true,
-	},
-    },
-    config = function(_, opts)
-	require('nvim-treesitter.install').prefer_git = true
-	require('nvim-treesitter.configs').setup(opts)
-    end,
+    event = { "BufReadPost", "BufNewFile" },
 }
+
+M.opts = {
+    ensure_installed = { "bash", "diff", "lua", "markdown", "rust", "go" },
+    sync_install = false,
+    auto_install = true,
+    ignore_install = { },
+    highlight = {
+	enable = true,
+	disable = { },
+	additional_vim_regex_highlighting = false,
+    },
+    indent = {
+	enable = true,
+    },
+}
+
+M.config = function(_, opts)
+    require('nvim-treesitter.install').prefer_git = true
+    require('nvim-treesitter.configs').setup(opts)
+end
+
+return M
