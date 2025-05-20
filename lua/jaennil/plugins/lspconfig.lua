b3f06341a4445b6d32b355f1da77dce94e1df5b8
jaennil <jaennil@yandex.ru>
2025-05-20 21:15:31 +0300

    feat(lspconfig): pyright; omnisharp

--- lua/jaennil/plugins/lspconfig.lua
+++ lua/jaennil/plugins/lspconfig.lua
@@ -7,6 +7,10 @@ M.config = function()
     local lspconfig = require("lspconfig")
     lspconfig.rust_analyzer.setup {}
     lspconfig.gopls.setup {}
+    lspconfig.pyright.setup {}
+    lspconfig.omnisharp.setup({
+        cmd = { "OmniSharp", "--languageserver", "--hostPID", tostring(vim.fn.getpid()) },
+    })
 end
 
 return M
