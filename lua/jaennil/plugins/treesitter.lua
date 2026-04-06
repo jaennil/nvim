e26bfb4e9ec7d798496a9d98d4e140724bf479f8
jaennil <jaennil@yandex.ru>
2026-04-06 13:57:30 +0300

    fix: update treesitter setup

--- lua/jaennil/plugins/treesitter.lua
+++ lua/jaennil/plugins/treesitter.lua
@@ -1,14 +1,27 @@
-return {
+local M = {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     event = { "BufReadPost", "BufNewFile" },
-    config = function()
-        require('nvim-treesitter').install({ "bash", "diff", "lua", "markdown", "rust", "go" })
-
-        vim.api.nvim_create_autocmd("FileType", {
-            callback = function()
-                pcall(vim.treesitter.start)
-            end,
-        })
-    end,
 }
+
+M.opts = {
+    ensure_installed = { "bash", "diff", "lua", "markdown", "rust", "go" },
+    sync_install = false,
+    auto_install = true,
+    ignore_install = {},
+    highlight = {
+        enable = true,
+        disable = {},
+        additional_vim_regex_highlighting = false,
+    },
+    indent = {
+        enable = true,
+    },
+}
+
+M.config = function(_, opts)
+    require("nvim-treesitter.install").prefer_git = true
+    require("nvim-treesitter.configs").setup(opts)
+end
+
+return M
