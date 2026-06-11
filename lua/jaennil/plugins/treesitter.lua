b5144d6359d1973274023963c1170cf94dbf1592
jaennil <jaennil@yandex.ru>
2026-06-11 13:39:39 +0300

    fix(nvim): update treesitter for nvim 0.12

--- lua/jaennil/plugins/treesitter.lua
+++ lua/jaennil/plugins/treesitter.lua
@@ -20,8 +20,23 @@ M.opts = {
 }
 
 M.config = function(_, opts)
-    require("nvim-treesitter.install").prefer_git = true
-    require("nvim-treesitter.configs").setup(opts)
+    local treesitter = require("nvim-treesitter")
+
+    treesitter.setup()
+    treesitter.install(opts.ensure_installed)
+
+    vim.api.nvim_create_autocmd("FileType", {
+        group = vim.api.nvim_create_augroup("jaennil-treesitter", { clear = true }),
+        callback = function(event)
+            if opts.highlight.enable then
+                local ok = pcall(vim.treesitter.start, event.buf)
+
+                if ok and opts.indent.enable then
+                    vim.bo[event.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+                end
+            end
+        end,
+    })
 end
 
 return M
